551fd5de2ba8db3ad0388c75bcda4df8c771a00b
justinchou <jackychou315@gmail.com>
2013-11-27 23:12:56 +0800

    add another rank method: Same Score Rank Depends On TimeStamp (First Come Rank Good)

--- ZsetRankList/apposition.js
+++ ZsetRankList/apposition.js
@@ -14,57 +14,89 @@ var setScore = function(key,u_id,score){
     client.quit();
 };
 
-var getRank = function(key,u_id,score,cb){
+var setScoreTimestamp = function(key,u_id,score){
     var client = redis.createClient();
     client.on("error", function (err) {
         console.log("Error " + err);
         return;
     });
-    client.zrevrank(key,u_id,function(err,index){
-        client.zcount(key,score,score,function(err,count){
-            if (count > 1){
-                client.zrevrangebyscore(key,score,score,function(err,list){
-                    for(i=0; i<count; i++){
-                        if(u_id == list[i]){
-                            cb(index-i+1);
-                            break;
-                        }
-                    };
-                    if(i>=count) cb(index);
-                    client.quit();
-                });
-            } else {
-                cb(index);
-                client.quit();
-            }
-        });
+	var time = new Date().valueOf() / 1000;
+	score += time / 1000000000;
+	client.zadd(key,u_id,score,redis.print);
+	client.quit();
+};
+
+var getRank = function(key,u_id,score,cb){
+	var client = redis.createClient();
+    client.on("error", function (err) {
+        console.log("Error " + err);
+        return;
     });
+
+	if(typeof score == "function"){
+	    cb = score;
+		client.zrevrank(key,u_id,function(err,index){
+		    cb(parseInt(index));
+			client.quit();
+		});
+	} else {
+		client.zrevrank(key,u_id,function(err,index){
+			client.zcount(key,score,score,function(err,count){
+				if (count > 1){
+					client.zrevrangebyscore(key,score,score,function(err,list){
+						for(i=0; i<count; i++){
+							if(u_id == list[i]){
+								cb(parseInt(index-i+1));
+								break;
+							}
+						};
+						if(i>=count) cb(parseInt(index));
+						client.quit();
+					});
+				} else {
+					cb(parseInt(index));
+					client.quit();
+				}
+			});
+		});
+	}
 };
 
 var reHashArray = function(arr,u_id,score){
-    var len = arr.length;
-    var cursor = 0;
-    var index = 1;
-    var reHashObject = {};
-    var key, value;
-    var exchange = true,
-        exclude = false;
-    for(;cursor<len;cursor+=2){
-        key = arr[cursor];
-        value = parseInt(arr[cursor+1]);
-        if(exchange && score == value){
-            reHashObject[index] = {"u_id":u_id,"score":score};
-            exchange = false;
-            exclude = true;
-            index++;
-        }
-        if(exclude && u_id == key){
-            continue;
-        }
-        reHashObject[index] = {"u_id":key,"score":parseInt(value)};
-        index++;
-    }
-    return reHashObject;
+	var len = arr.length;
+	var cursor = 0;
+	var index = 1;
+	var reHashObject = {};
+	var key, value;
+
+	if(undefined == u_id && undefined == score){
+		for(;cursor<len;cursor+=2){
+			key = arr[cursor];
+			value = parseInt(arr[cursor+1]);
+			reHashObject[index] = {"u_id":key,"score":parseInt(value)};
+			index++;
+		}
+	} else {
+		var exchange = true,
+			exclude = false;
+		for(;cursor<len;cursor+=2){
+			key = arr[cursor];
+			value = parseInt(arr[cursor+1]);
+			if(exchange && score == value){
+				reHashObject[index] = {"u_id":u_id,"score":score};
+				exchange = false;
+				exclude = true;
+				index++;
+			}
+			if(exclude && u_id == key){
+				continue;
+			}
+			reHashObject[index] = {"u_id":key,"score":parseInt(value)};
+			index++;
+		}
+	}
+
+	return reHashObject;
 }
 
 var getRankList = function(key,u_id,score,amount,cb){
@@ -73,16 +105,29 @@ var getRankList = function(key,u_id,score,amount,cb){
         console.log("Error " + err);
         return;
     });
-    var conditions = [key,0,amount,'WITHSCORES'];
-    client.zrevrange(conditions,function(err,list){
-        var hashList = reHashArray(list,u_id,score);
-        cb(hashList);
-        client.quit();
-    });
+
+	if(typeof score == "function"){
+		cb = score;
+		amount = u_id;
+		var conditions = [key,0,amount,'WITHSCORES'];
+		client.zrevrange(conditions,function(err,list){
+			var hashList = reHashArray(list);
+			cb(hashList);
+			client.quit();
+		});
+	} else {
+		var conditions = [key,0,amount,'WITHSCORES'];
+		client.zrevrange(conditions,function(err,list){
+			var hashList = reHashArray(list,u_id,score);
+			cb(hashList);
+			client.quit();
+		});
+	}
 };
 
 module.exports = {
-    "getRank": getRank,
-    "setScore": setScore,
+	"setScore": setScore,
+	"setScoreTimestamp": setScoreTimestamp,
+	"getRank": getRank,
     "getRankList": getRankList
 };
--- ZsetRankList/tests/example.js
+++ ZsetRankList/tests/example.js
@@ -1,24 +1,46 @@
 var ZsetRankList = require("../index.js");
+var key, u_id, score, i;
+var amount = 30;
 
-var key = "key";
-var u_id = "u_key143";
-var score = 27;
-var i;
-for (i=0;i<20;i++){
-//    client.zadd("key",i*2+1,"u_key"+(i*11));
-//    client.zadd("key",i*2+1,"u_key"+(i*12));
-//    client.zadd("key",i*2+1,"u_key"+(i*13));
-//    ZsetRankList.setScore(key,i*2+1,"u_key"+(i*11));
-//    ZsetRankList.setScore(key,i*2+1,"u_key"+(i*12));
-//    ZsetRankList.setScore(key,i*2+1,"u_key"+(i*13));
-//    ZsetRankList.setScore(key,i*2+1,"u_key"+(i*14));
-//    ZsetRankList.setScore(key,i*2+1,"u_key"+(i*15));
+
+/**  Part I: Same Score Show The Same Rank (But Occupied Multi)  **/
+{
+	key = "keyI";
+	u_id = "u_key143";
+	score = 27;
+	for (i=0;i<20;i++){
+		ZsetRankList.setScore(key,i*2+1,"u_key"+(i*11));
+		ZsetRankList.setScore(key,i*2+1,"u_key"+(i*12));
+		ZsetRankList.setScore(key,i*2+1,"u_key"+(i*13));
+		ZsetRankList.setScore(key,i*2+1,"u_key"+(i*14));
+		ZsetRankList.setScore(key,i*2+1,"u_key"+(i*15));
+	}
+
+	ZsetRankList.getRank(key,u_id,score,function(rank){
+		console.log(u_id," score is:", score, " rank is:", rank)
+	});
+	ZsetRankList.getRankList(key,u_id,score,amount,function(list){
+		console.log("rank list:\n",list);
+	});
 }
 
-ZsetRankList.getRank(key,u_id,score,function(rank){
-    console.log(u_id," score is:", score, " rank is:", rank)
-});
-ZsetRankList.getRankList(key,u_id,score,30,function(list){
-    console.log("rank list:\n",list);
-});
 
+/**  Part II: Same Score Rank Depends On TimeStamp (First Come Rank Good)  **/
+{
+	key = "keyII";
+	u_id = "u_key143";
+	for (i=0;i<20;i++){
+		ZsetRankList.setScoreTimestamp(key,i*2+1,"u_key"+(i*11));
+		ZsetRankList.setScoreTimestamp(key,i*2+1,"u_key"+(i*12));
+		ZsetRankList.setScoreTimestamp(key,i*2+1,"u_key"+(i*13));
+		ZsetRankList.setScoreTimestamp(key,i*2+1,"u_key"+(i*14));
+		ZsetRankList.setScoreTimestamp(key,i*2+1,"u_key"+(i*15));
+	}
+
+	ZsetRankList.getRank(key,u_id,function(rank){
+		console.log(u_id," rank is:", rank)
+	});
+	ZsetRankList.getRankList(key,amount,function(list){
+		console.log("rank list:\n",list);
+	});
+}
